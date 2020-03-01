a7ad41b241db293ee7cbef9be01b32237b01dcaf
StavrosAnastopoulos <8824406+StavrosAnastopoulos@users.noreply.github.com>
2020-03-01 20:47:59 +0200

    Updates

---
+++ precache-manifest.a10bce78ce6c3dab9235eb8c974b90df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b26496f882fb7167576aeafde20a736",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dd109f93dc151cdec694",
+    "url": "/Portfolio/static/css/3.38a194d4.chunk.css"
+  },
+  {
+    "revision": "21e3e87fdf630a0c88aa",
+    "url": "/Portfolio/static/css/4.85a142ad.chunk.css"
+  },
+  {
+    "revision": "aa7dcfafa72bf29f8bef",
+    "url": "/Portfolio/static/css/main.46b2db65.chunk.css"
+  },
+  {
+    "revision": "21b0da805fdd3dcb2d67",
+    "url": "/Portfolio/static/js/2.75617973.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/Portfolio/static/js/2.75617973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd109f93dc151cdec694",
+    "url": "/Portfolio/static/js/3.65b4d911.chunk.js"
+  },
+  {
+    "revision": "21e3e87fdf630a0c88aa",
+    "url": "/Portfolio/static/js/4.09bb196a.chunk.js"
+  },
+  {
+    "revision": "aa7dcfafa72bf29f8bef",
+    "url": "/Portfolio/static/js/main.fb8b3c93.chunk.js"
+  },
+  {
+    "revision": "75bf8bf8a30ab644a2af",
+    "url": "/Portfolio/static/js/runtime-main.a504b688.js"
+  }
+]);
